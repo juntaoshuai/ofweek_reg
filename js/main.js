f4206895b3112370240a9772bb459de1773ed5c5
juntaoshuai <juntaos0917@163.com>
2016-11-30 23:32:15 +0800

    validate2

--- js/main.js
+++ js/main.js
@@ -40,8 +40,9 @@ $(".form-control .sex").click(function(){
 
 //全局正则配置
 var RegConfig={
-	'mobile':/^[0-9]*[-]*[0-9]*$/,
-	'email':/^[A-Za-z0-9-_\.]+\@([A-Za-z0-9-_]+\.)+[A-Za-z0-9]{2,6}$/
+	'mobile':/^(((13[0-9]{1})|(14[0-9]{1})|(17[0]{1})|(15[0-3]{1})|(15[5-9]{1})|(18[0-9]{1}))+\d{8})$/,
+	'email':/^[A-Za-z0-9-_\.]+\@([A-Za-z0-9-_]+\.)+[A-Za-z0-9]{2,6}$/,
+    'pwd':/^(?=[\w\W])[^*]{6,20}$/
 }
 
 
@@ -49,46 +50,64 @@ var RegConfig={
 //验证不通过
 function noPass(msg,obj){
 	var $msg = $('<span class="nopass"></span>');
-    $(obj).addClass("error").siblings(".nopass,.pass").remove();
-	$msg.html(msg).insertAfter($(obj));
+    $(obj).addClass("error").siblings(".nopass,.pass,.input-tips").remove();
+	$msg.html(msg).appendTo($(obj).closest(".form-control"));
 }
 //验证通过
-function Pass(obj){
+function pass(obj){
 	var $pass = $('<span class="pass"></span>');
-    $(obj).remove("error").siblings(".nopass,.pass").remove();
-    $pass.insertAfter($(obj));
+    $(obj).remove("error").siblings(".nopass,.pass,.input-tips").remove();
+    $pass.appendTo($(obj).closest(".form-control"));
+
 }
 
 //正则表达式验证
 function RegCheck(obj,regular){
     var objval=$(obj).val();
-   if(!regular.test(objval)){
+   if(regular.test(objval)){
         return true;
    }else{
         return false;
    }
 }
-//ajax验证
-var isHave;
-function remoteCheck(obj,url,string){
+//手机或邮箱实时验证
+function remoteCheckMobileEmail(obj,url,msg){
     var objval=$(obj).val();
-    
-           
     $.ajax({
-            
             url:url,
             data:{name:objval},
             success:function(data){
                 var data=$.parseJSON(data);
                 if(data.status==1){
-                   noPass(string, obj);
+                   noPass(msg, obj);
                 }else{
-                   pass();
-
+                    //输入手机时，出现图片验证码,邮箱则隐藏
+                    if(RegCheck(obj,RegConfig.mobile)){
+                        $(obj).closest(".form-group").next().show();
+                    }else{
+                        $(obj).closest(".form-group").next().hide();
+                    }
+                    pass(obj);
                 }
             }
         });
-    return isHave;
+}
+
+//手机或邮箱实时验证
+function remoteCheck(obj,url,msg){
+    var objval=$(obj).val();
+    $.ajax({
+            url:url,
+            data:{name:objval},
+            success:function(data){
+                var data=$.parseJSON(data);
+                if(data.status==1){
+                    noPass(msg, obj);
+                }else{
+                    pass(obj);
+                }
+            }
+        });
 }
 
 //账号注册验证
@@ -112,21 +131,133 @@ function checkPhoneAndEmail(obj) {
     $(obj).blur(function() {
         if (checkEmpty(obj)) {
             noPass("请输入邮箱或手机",obj);
-        }else if(checkLength(obj,50)){
+        }else if(checkLength(obj,50)){      
             noPass("邮箱长度超过限定",obj);
-        }else if(RegCheck(obj,RegConfig.email) || RegCheck(obj,RegConfig.mobile)){
+        }else if(!(RegCheck(obj,RegConfig.email)) && !(RegCheck(obj,RegConfig.mobile))){
             noPass("邮箱或手机格式不正确",obj);
-        }else if(remoteCheck(obj,"http://www.baidu.com")){
-            noPass("该邮箱/手机已经存在，请更换或 <a href='#'>登录</a>",obj);
         }else{
-       
-            remoteCheck(obj,"js/data.txt","该邮箱/手机已经存在，请更换或 <a href='#'>登录</a>")
+            remoteCheckMobileEmail(obj,"js/data.txt","该邮箱/手机已经存在，请更换或 <a href='#'>登录</a>");
         }
-        /*if(remoteCheck(obj,"js/data.txt")){
-            alert(123)
-            noPass("该邮箱/手机已经存在，请更换或 <a href='#'>登录</a>",obj);
-        }*/
+       
+    });
 
+}
+function checkImgCode(obj) {
+    $(obj).blur(function() {
+        if (checkEmpty(obj)) {
+            noPass("请输入验证码",obj);
+        }else{
+            remoteCheck(obj,'js/data.txt',"您输入的验证码有误");      
+        }
+       
+    });
+
+}
+
+//验证码
+//验证码倒计时
+$("#getcode").click(function(){
+    /*
+        上面邮箱/手机为空   按钮灰色无法点击
+        上面图片验证码为空   
+        邮箱/手机不正确    
+        图片验证码不正确    
+    */
+    if($(this).closest(".form-group").prevAll(".pass").length!=2){
+       // $(this).attr("disabled","disabled");
+       $(this).css("background","#ddd");
+       return false;
+    }
+    alert("come");
+
+
+    var iCount=60,
+        $this=$(this),
+        timer=null;
+    var url='';
+    if($this.val()=="获取验证码")
+    {
+        var mobile=$("#account").val();
+        if(mobile)
+        {
+            url="http://www.ieduchina.com/register.html?do=mobilecode";
+        }
+        else
+        {
+            mobile=$("#mobile").val();
+            url="http://www.ieduchina.com/index.php?m=member&c=index&a=complete&do=mobilecode";
+        }   
+
+        if(mobile=="")
+        {
+            return false;
+        }
+        
+        var myreg=/^(((13[0-9]{1})|(14[0-9]{1})|(17[0]{1})|(15[0-3]{1})|(15[5-9]{1})|(18[0-9]{1}))+\d{8})$/; 
+        if(mobile.length !=11)
+        {
+            return false;
+        }
+        if(!myreg.test(mobile))
+        {
+            return false;
+        }   
+        
+        $.post(url,{'mobile':mobile},function(data){
+            if(data==0){$this.parent().find("label.error").html('验证码发送手机失败!');return false;}
+            if(data==1){$this.parent().find("label.error").html('验证码已发送，请查看手机!');}  
+            if(data==2){$this.parent().find("label.error").html('请更换手机号!');return false;}
+            if(data==3){$this.parent().find("label.error").html('该手机今日发送已达到上限!');return false;}
+        });
+    } 
+    var mobile=$("#account").val();
+    if(mobile=="")
+    {
+        return false;
+    }
+    
+    $(this).attr("disabled","disabled");
+    $this.val(iCount+"s后重新发送"); 
+    timer=setInterval(function(){
+        iCount--;
+        if(iCount==0){
+            $this.val("获取验证码");
+            clearInterval(timer); 
+           $this.attr("disabled",false);
+        }else{
+           $this.val(iCount+"s后重新发送");      
+        }
+        
+    },1000);
+    
+ });
+
+//密码验证
+function checkPwd(obj) {
+    $(obj).blur(function() {
+        if (checkEmpty(obj)) {
+            noPass("请输入密码",obj);
+        }else if(!RegCheck(obj,RegConfig.pwd)){
+            noPass("支持6-20位数字、字母和标点符号",obj);
+        }else{
+            pass(obj);
+        }
+       
+    });
+
+}
+
+//确认密码
+function checkRePwd(obj) {
+    $(obj).blur(function() {
+        if (checkEmpty(obj)) {
+            noPass("请再次输入密码",obj);
+        }else if($("#pwd").val()!=$(obj).val()){
+            noPass("您2次输入的密码不一致",obj);
+        }else{
+            pass(obj);
+        }
+       
     });
 
 }
@@ -151,6 +282,9 @@ function validateAwards() {
 var valiate={
 	regform:function(){
 		checkPhoneAndEmail("#account");
+        checkImgCode("#img_code");
+        checkPwd("#pwd");
+        checkRePwd("#repwd");
 
 	
 	
